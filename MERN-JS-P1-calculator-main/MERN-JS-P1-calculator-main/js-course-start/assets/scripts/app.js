2c0abc66435b5e6028e19ec0baead1ec623b33ce
ermina <ermina_b@hotmail.com>
2024-07-14 15:01:20 +0200

    Adding functions, chaning button colors, linking header to web site

--- MERN-JS-P1-calculator-main/MERN-JS-P1-calculator-main/js-course-start/assets/scripts/app.js
+++ MERN-JS-P1-calculator-main/MERN-JS-P1-calculator-main/js-course-start/assets/scripts/app.js
@@ -1,1 +1,25 @@
-const broj = 1;
+function addition(a, b){
+    return (a+b);
+}
+
+function subtraction(a, b){
+    return (a-b);
+}
+
+function division(a, b){
+    return (a/b);
+}
+
+function multiplication(a, b){
+    return (a*b);
+}
+
+
+console.log(addition(2, 3))
+
+console.log(subtraction(2, 3))
+
+console.log(division(2, 3))
+
+console.log(multiplication(2, 3))
+
